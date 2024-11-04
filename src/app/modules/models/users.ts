d8ecdc9e4b3ad696991bcc148a50252b7e428e8c
Faruk Taiwo <faruk.taiwo@ubagroup.com>
2024-11-04 13:36:26 +0100

    type interface model for user data

---
+++ src/app/modules/models/users.ts
@@ -0,0 +1,28 @@
+export interface Users {
+    organizations: string,
+    username: string,
+    email: string,
+    phone_number: string,
+    date_joined: string,
+    status: string,
+    personal_information: any,
+    education: {},
+    employment: {},
+    socials: any,
+    guarantor: any 
+  }
+
+
+// export const users = {
+//     organizations: [],
+//     username: '',
+//     email: '',
+//     phone_number: '',
+//     date_joined: '',
+//     status: '',
+//     personal_information: {},
+//     education: [],
+//     employment: [],
+//     socials: {},
+//     guarantor: {} 
+// }
