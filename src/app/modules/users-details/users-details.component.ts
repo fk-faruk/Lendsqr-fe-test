fb819c087f140faf188d8badae216a29c469a109
Faruk Taiwo <faruk.taiwo@ubagroup.com>
2024-11-06 08:44:34 +0100

    mapped passed user details from local storage to template

--- src/app/modules/users-details/users-details.component.ts
+++ src/app/modules/users-details/users-details.component.ts
@@ -11,8 +11,42 @@ export class UsersDetailsComponent {
   constructor(private route : Router) {}
 
 
+  userInfo : any 
+
+
 
   routeTo() {
     this.route.navigate(['/dashboard/users']);
   }
+
+
+  getUserInfo() {
+   const dataFromLS = localStorage.getItem('user-profile')
+   const parsedData =  dataFromLS ? JSON.parse(dataFromLS) : null;
+
+   this.userInfo = parsedData
+
+   console.log('User info:', this.userInfo);
+  //  console.log(`User info: ${this.userInfo}`);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+ngOnInit() {
+    this.getUserInfo()
+}
+
+
+
+
+
 }
