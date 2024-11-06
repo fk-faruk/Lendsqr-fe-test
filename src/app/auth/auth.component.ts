dfd823916fe58871a156d641076f90803dc6d08b
Faruk Taiwo <faruk.taiwo@ubagroup.com>
2024-11-06 08:38:54 +0100

    added the show and hide password feature

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+  EmailValidator,
+  FormBuilder,
+} from '@angular/forms';
+import { AuthserviceService } from '../services/authservice.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,17 +18,36 @@ import { Router } from '@angular/router';
 export class AuthComponent {
   ubaIcon: string = '/assets/UBA_icon.png';
 
-  constructor(private router: Router) {}
+  userAuth!: FormGroup 
+
+
+
+  constructor(private router: Router, private fb : FormBuilder, private auth : AuthserviceService) {}
   
-  routeToDashboard() {
-    this.router.navigate(['/dashboard/users']);
+  submitForm()   {
+
+    const {email , password} = this.userAuth.value
+    localStorage.setItem('adminName', email)
+
+    this.auth.loggedIn(this.userAuth.value)
+
+    // this.router.navigate(['/dashboard/users']);
   }
 
 
 
+  formvalidation() {
+    this.userAuth = this.fb.group({
+      email: ['', [Validators.required, Validators.email ]], // Email control
+      password: ['', [Validators.required]] // Password control
+    });
+  }
+
+  
+
  
 
   ngOnInit() {
-
+      this.formvalidation()
   }
 }
